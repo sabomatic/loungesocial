87a0541cd0d7e6505b0f0d38c6689f044e60db46
Rodrigo Haenggi <rodrigo@codegestalt.com>
2017-03-13 17:47:44 +0100

    connect controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'hotspots#welcome'
+  post '/find', to: 'hotspots#find', as: 'find'
+
   resources :hotspots
-
-  get 'screens/landing'
-
-  get 'screens/create'
-
-  get 'screens/status'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
